9a021da3490aad3a6bad0dcf8602ced1c0ccf6e7
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-16 05:56:56 -0500

    PolarisProvider test

--- src/foundation/App/tests/App.test.tsx
+++ src/foundation/App/tests/App.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {AppProvider} from '@shopify/polaris';
 
 import {App} from '../App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />);
+  });
+
+  it('renders with PolarisProvider', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(AppProvider)).toBeTruthy();
   });
 });
