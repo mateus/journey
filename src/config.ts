6dc768f5fb965450c795812e6a1c2162479ee66f
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-16 16:22:21 -0500

    Custom dev mode ui

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-process-env */
+
+export const isDevelopment = process.env.NODE_ENV === 'development';
+export const publicURL = process.env.PUBLIC_URL;
--- src/foundation/App/App.tsx
+++ src/foundation/App/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Favicon from 'react-favicon';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import {AppProvider} from '@shopify/polaris';
 import {TravelHistory} from 'views';
+import {isDevelopment, publicURL} from 'config';
+
+import './App.scss';
 
 export function App() {
   return (
-    <AppProvider i18n={enTranslations}>
-      <TravelHistory />
-    </AppProvider>
+    <>
+      <AppProvider i18n={enTranslations}>
+        {isDevelopment && <DevelopmentHead />}
+        <TravelHistory />
+      </AppProvider>
+    </>
+  );
+}
+
+function DevelopmentHead() {
+  return (
+    <>
+      <Favicon url={`${publicURL}/favicons/development/favicon.ico`} />
+      <aside className="DevStatus">
+        <strong>development</strong> mode
+      </aside>
+    </>
   );
 }
