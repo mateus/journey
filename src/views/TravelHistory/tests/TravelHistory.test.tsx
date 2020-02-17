5bbd8e934f9fb7de3dd579127db4daef178a4c9c
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-16 22:32:04 -0500

    Add trip button

--- src/views/TravelHistory/tests/TravelHistory.test.tsx
+++ src/views/TravelHistory/tests/TravelHistory.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Page} from '@shopify/polaris';
+import {act} from 'react-dom/test-utils';
 import {mountWithPolarisProvider} from 'utilities/tests';
 
 import {TravelHistory} from '../TravelHistory';
+import {NewTripCard, RandomQuote, UpcomingTripsCard} from '../components';
 
 describe('<TravelHistory />', () => {
   it('renders <Page />', () => {
@@ -10,6 +12,22 @@ describe('<TravelHistory />', () => {
     expect(wrapper.find(Page).exists()).toBeTruthy();
     expect(wrapper.find(Page).props()).toMatchObject({
       title: 'Travel History',
+    });
+  });
+
+  describe('<NewTripCard />', () => {
+    it('is closed on first load', () => {
+      const wrapper = mountWithPolarisProvider(<TravelHistory />);
+      expect(wrapper.find(NewTripCard).exists()).toBeFalsy();
+    });
+
+    it('renders when clicking "Add trip" action from Page', async () => {
+      const wrapper = mountWithPolarisProvider(<TravelHistory />);
+      await act(async () => {
+        await wrapper.find(Page)!.prop('primaryAction')!.onAction!();
+        await wrapper.update();
+      });
+      expect(wrapper.find(NewTripCard).exists()).toBeTruthy();
     });
   });
 });
