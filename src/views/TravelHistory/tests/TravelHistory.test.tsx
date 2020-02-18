36eee784625378e7df19004b5a9fc8246c7d248e
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-17 22:13:48 -0500

    TravelHistory tests

--- src/views/TravelHistory/tests/TravelHistory.test.tsx
+++ src/views/TravelHistory/tests/TravelHistory.test.tsx
@@ -3,9 +3,17 @@ import {Card, Page} from '@shopify/polaris';
 import {act} from 'react-dom/test-utils';
 
 import {mountWithPolarisProvider} from 'utilities/tests';
+import {mockTrip} from 'utilities';
+import {Trip} from 'types';
 
+import {sortByStartDateAsc} from '../utilities';
 import {TravelHistory, TravelHistoryProps} from '../TravelHistory';
-import {ManageTripCard, RandomQuote, UpcomingTripsCard} from '../components';
+import {
+  ManageTripCard,
+  TripDetailsCard,
+  RandomQuote,
+  UpcomingTripsCard,
+} from '../components';
 
 import {mockTrips} from './fixtures/mockTrips';
 
@@ -32,9 +40,18 @@ describe('<TravelHistory />', () => {
     ).toBeTruthy();
   });
 
-  it('renders <UpcomingTripsCard />', () => {
-    const wrapper = mountWithPolarisProvider(<TravelHistory {...mockProps} />);
-    expect(wrapper.find(UpcomingTripsCard).exists()).toBeTruthy();
+  describe('<UpcomingTripsCard />', () => {
+    it('renders list of not completed trips', () => {
+      const wrapper = mountWithPolarisProvider(
+        <TravelHistory {...mockProps} />,
+      );
+      const upcoming = mockProps
+        .trips!.filter(({completed}) => !completed)
+        .sort(sortByStartDateAsc);
+      expect(wrapper.find(UpcomingTripsCard).prop('list')).toStrictEqual(
+        upcoming,
+      );
+    });
   });
 
   describe('<ManageTripCard />', () => {
@@ -69,6 +86,42 @@ describe('<TravelHistory />', () => {
           disabled: true,
         },
       });
+    });
+  });
+
+  describe('<TripDetailsCard />', () => {
+    it('renders one for each trip', () => {
+      const wrapper = mountWithPolarisProvider(
+        <TravelHistory {...mockProps} />,
+      );
+      expect(wrapper.find(TripDetailsCard)).toHaveLength(mockTrips.length);
+    });
+
+    it('renders with the Trip prop set', () => {
+      const [first, second, third] = [
+        mockTrip({endDate: new Date('01/01/2019')}),
+        mockTrip({endDate: new Date('02/02/2019')}),
+        mockTrip({endDate: new Date('01/01/2020')}),
+      ];
+
+      const wrapper = mountWithPolarisProvider(
+        <TravelHistory trips={[first, second, third]} />,
+      );
+      expect(
+        wrapper.find(TripDetailsCard).map((node) => node.props()),
+      ).toStrictEqual([third, second, first]);
+    });
+
+    it('renders a separator for each group of year', () => {
+      const trips: Trip[] = [
+        mockTrip({endDate: new Date('01/01/2020')}),
+        mockTrip({endDate: new Date('01/01/2020')}),
+        mockTrip({endDate: new Date('01/01/2019')}),
+        mockTrip({endDate: new Date('01/01/2019')}),
+        mockTrip({endDate: new Date('01/01/2018')}),
+      ];
+      const wrapper = mountWithPolarisProvider(<TravelHistory trips={trips} />);
+      expect(wrapper.find('.Separator')).toHaveLength(3);
     });
   });
 });
