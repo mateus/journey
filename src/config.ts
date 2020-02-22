3720f92ad7c3c192ef12acdcd3a517db2a760f5f
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-22 03:33:42 -0500

    Moving env to Build phase

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-process-env */
 
+console.log('From config', process.env);
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const publicURL = process.env.PUBLIC_URL;
