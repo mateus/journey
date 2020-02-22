c1492bd4e213546540359c30fa066cffde765fe4
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-21 22:01:01 -0500

    Add Firebase config

--- src/foundation/App/App.tsx
+++ src/foundation/App/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import {AppProvider} from '@shopify/polaris';
+import {useAuthState} from 'react-firebase-hooks/auth';
 
+import {auth} from 'utilities/firebase';
 import {TravelHistory} from 'views';
 import {isDevelopment} from 'config';
 
 import {DevelopmentHead, TopNav, Footer} from './components';
 
 export function App() {
+  const [user, initialising, error] = useAuthState(auth);
   // Add a Single Query Request for all the pages
 
   return (
