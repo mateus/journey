6e9424301201c7d6a5645460357d09ba2e0ec201
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-22 01:04:46 -0500

    Tests for Frame

---
+++ src/foundation/Frame/tests/Frame.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import {Frame} from '../Frame';
+import {TopNav} from '../../TopNav';
+import {Footer} from '../../Footer';
+
+describe('<Frame />', () => {
+  const MyComponent = () => {
+    return <h1>Component</h1>;
+  };
+
+  it('renders <TopNav />', () => {
+    const wrapper = shallow(
+      <Frame>
+        <MyComponent />
+      </Frame>,
+    );
+    expect(wrapper.find(TopNav)).toExist();
+  });
+
+  it('renders children', () => {
+    const wrapper = shallow(
+      <Frame>
+        <MyComponent />
+      </Frame>,
+    );
+    expect(wrapper.find(MyComponent)).toExist();
+  });
+
+  it('renders <Footer />', () => {
+    const wrapper = shallow(
+      <Frame>
+        <MyComponent />
+      </Frame>,
+    );
+    expect(wrapper.find(Footer)).toExist();
+  });
+});
