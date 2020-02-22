c248de9075cb01d371c4bf4ccbeadc6a00a17593
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-21 23:47:01 -0500

    Login page draft

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import GoogleButton from 'react-google-button';
+import {Redirect} from 'react-router-dom';
+import {DisplayText, Stack, Spinner} from '@shopify/polaris';
+
+import {auth, provider} from 'utilities/firebase';
+
+import './Login.scss';
 
 export function Login() {
-  return <h1>Login page</h1>;
+  const [user, initialising, error] = useAuthState(auth);
+
+  const login = () => {
+    auth.signInWithPopup(provider);
+  };
+
+  const LoginFrame = ({children}: {children: React.ReactNode}) => {
+    return (
+      <div className="Login">
+        <Stack vertical alignment="center" distribution="center">
+          {children}
+        </Stack>
+      </div>
+    );
+  };
+
+  if (initialising) {
+    return (
+      <LoginFrame>
+        <Spinner accessibilityLabel="Initialising" size="large" color="teal" />
+      </LoginFrame>
+    );
+  }
+
+  if (error) throw new Error(error.message);
+
+  if (user) {
+    return <Redirect to={{pathname: '/travels'}} />;
+  }
+
+  return (
+    <LoginFrame>
+      <Stack vertical alignment="center" spacing="extraLoose">
+        <DisplayText size="extraLarge">Login</DisplayText>
+        <GoogleButton onClick={login} />
+      </Stack>
+    </LoginFrame>
+  );
 }
