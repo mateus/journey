4af8e15dfb2c1911f500fef8da8691d243f4a22b
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2020-02-25 01:12:41 -0500

    Rename views to pages

--- src/foundation/Routes/Routes.tsx
+++ src/foundation/Routes/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import {NotFound} from 'components';
-import {Login, TravelHistory} from 'views';
+import {Login, TravelHistory} from 'pages';
 
 import {ProtectedRoute} from '../ProtectedRoute';
 
